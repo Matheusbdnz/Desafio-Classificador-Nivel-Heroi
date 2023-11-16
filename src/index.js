91f8428994eb4863161e978425bd40904836be53
MatheusBdnz <matheusbdnz@outlook.com>
2023-11-15 22:42:15 -0300

    código do desafio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log("oi");
+// Desafio Classificador de nível de herói
+
+let nomeHeroi = "Jin"
+let expHeroi = 9.003
+
+if (expHeroi >= 0 && expHeroi <=1.000){
+   console.log("O herói de nome " + nomeHeroi + " está no nível bronze ")
+}
+
+else if (expHeroi >=1.001 && expHeroi <=2.000){
+  console.log("O herói de nome " + nomeHeroi + " está no nível ferro ")
+
+}
+
+else if (expHeroi >=2.001 && expHeroi <=5.000){
+    console.log("O herói de nome " + nomeHeroi + " está no nível Prata ")
+  
+  }
+
+  else if (expHeroi >=5.001 && expHeroi <=8.000){
+    console.log("O herói de nome " + nomeHeroi + " está no nível Ouro ")
+  
+  } 
+
+  else if (expHeroi >=8.001 && expHeroi <=9.000){
+    console.log("O herói de nome " + nomeHeroi + " está no nível Ascendente ")
+  
+  }
+
+  else if (expHeroi >=9.001 && expHeroi <=10.000){
+    console.log("O herói de nome " + nomeHeroi + " está no nível Imortal ")
+  
+  }
+
+  else if (expHeroi >=10.001){
+    console.log("O herói de nome " + nomeHeroi + " está no nível Radiante ")
+  
+  }
+
